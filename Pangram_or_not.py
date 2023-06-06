c2b757b8d9bfa7520748596cf8d9ec988850a829
vanaja7 <134764883+vanaja7@users.noreply.github.com>
2023-06-06 15:23:38 +0530

    n=input().lower().split() l=''.join(n) c=0 for i in 'abcdefghijklmnopqrstuvwxyz':     if i in l:          c=c+1 if c==26:     print("True") else:     print("False")

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,9 +1,9 @@
 n=input().lower().split()
-l="".join(n)
+l=''.join(n)
 c=0
 for i in 'abcdefghijklmnopqrstuvwxyz':
     if i in l:
-        c=c+1
+         c=c+1
 if c==26:
     print("True")
 else:
